e5fa7d036b5da03e35930f744950cffced74475a
Caldwell <caldwellc1@appd.appstate.edu>
2017-09-22 16:21:01 -0400

    progress

--- AI PA 2.py
+++ AI PA 2.py
@@ -1,74 +1,77 @@
-
 import pandas as pd
-df = pd.read_csv('breadth_first_search_train.csv')
-print(df['board'][0])
-print(df['breadth_first_search'][0])
+# print(df['board'][0])
+# print(df['breadth_first_search'][0])
 # split = df['board'].split(';')
 
 
-def actions(state, action_taken, animal, house):
-    # if animal there grab it
-    # if house there, see if animal is in car
-    if action_taken == 'd':
-        new_state = [state[0] + 1, state[0] + 1, state[0] + 1, state[0] + 1,]
-    elif action_taken == 'l':
-        new_state = state + 1
-    elif action_taken == 'r':
-        new_state = state + 1
-    elif action_taken == 'u':
-        new_state = state + 1
+def breadth_first_search(problem):
+    node = Node(problem.init)
+    explored = set()
+    frontier = [node]
+    while frontier:
+        node = frontier.pop(0)
+        if problem.goal_test(node.state):
+            return node #solution(node)
+        explored.add(node.state)
+        for action in problem.actions(node.state):
+            child = Node(problem, node, action)
+            if child.state #not in explored or frontier:
+                frontier.append(child) #frontier?
+            elif child.state #in frontier higher cost:
+                #replace frontier node with child
+    return None
 
 
-def goal_test(state):
-    if state == []:
-        return 1
-    return 0
+class Problem:
+    def __init__(self, board):
+        print(board)
+        # split board in to a list/matrix of rows and columns
+        matrix_board = []
+        row = []
+        for j in range(len(board)):
+            if ord(board[j]) != 59:
+                row.append(board[j])
+            else:
+                matrix_board.append(row)
+                row = []
+        # get location of ^ for x and y
+        x = 0
+        y = 0
+        for k in range(len(matrix_board)):
+            row = matrix_board[k]
+            for m in range(len(row)):
+                if row[m] == '^':
+                    x = m
+                    y = k
+        in_car = []
+        on_board = []
+        # get animals on board
+        for i in range(len(board)):
+            if ord(board[i]) >= 97 and ord(board[i]) <= 122:
+                on_board.append(board[i])
+        self.init = [x, y, in_car, on_board]
+        self.board = matrix_board
+        self.path_cost = 0
+
+    def actions(self, state):
+        return ['d', 'l', 'r', 'u']
 
 
-def start_state(board):
-    # split board in to a list/matrix of rows and columns
-    matrix_board = []
-    row = []
-    for j in range(len(board[1])):
-        if ord(board[1][j]) != 59:
-            row.append(board[1][j])
-        else:
-            matrix_board.append(row)
-            row = []
-    # get location of ^ for x and y
-    x = 0
-    y = 0
-    in_car = []
-    on_board = []
-    # get animals on board
-    for i in range(len(board[1])):
-        if ord(board[1][i]) >= 97 or ord(board[1][i]) <= 122:
-            on_board.append(board[1][i])
-    return [x, y, in_car, on_board]
+    def result(self, state, action):
+        if action == 'd':
+            #see if can move, if so move, see if pet there or house
+        elif action == 'l':
+            new_state = state + 1
+        elif action == 'r':
+            new_state = state + 1
+        elif action == 'u':
+            new_state = state + 1
+        #return = new_state
 
 
-def breadth_first_search(board):
-    answer = ''
-    state = start_state(board)
-    node = {}
-    explored = {}
-    frontier = [state]
-    actions = ['d', 'l', 'r', 'u']
-    while goal_test(state) == 0:
-        if len(frontier) == 0:
-            return 'Fail'
-        if goal_test(state) == 1:
-            return state
-        explored.update(state)
-        for action in actions:
-            child = [board, state, action]
-            if child[1] in explored == False or child[1] in frontier:
-                frontier.append(child)
-            elif child[1] in frontier:
-                frontier[-1] = child
-    return answer
+    def goal_test(self, state):
+        return len(state[2]) == 0 and len(state[3]) == 0
 
-print(breadth_first_search(df['board'][0]))
 
 class Node:
     def __init__(self, state, parent=None, action=None, path_cost=0):
@@ -76,6 +79,12 @@ class Node:
         self.parent = parent
         self.action = action
         self.path_cost = path_cost
-        self.depth = 0
-        if parent:
-            self.depth = parent.depth + 1
+
+
+def main():
+    df = pd.read_csv('breadth_first_search_train.csv')
+    problem = Problem(df['board'][0])
+    print(breadth_first_search(problem))
+
+if __name__ == '__main__':
+    main()
